f2cd01f16786aef35434249ef0686ebcedc7299f
nadaalaa467 <n192004a@gmail.com>
2024-08-09 22:54:21 -0700

    first commit

---
+++ src/components/fragment/Attribution.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/scss/Attribution.scss';
+import {useSelector} from "react-redux";
+
+const Attribution = () => {
+    const {playing} = useSelector(state => state.musicReducer);
+    return (
+        <div className={"Attribution"}>
+       
+        </div>
+    );
+}
+
+export default Attribution;
---
+++ src/components/fragment/Developer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/scss/Developer.scss';
+import {IconButton} from "@material-ui/core";
+import AvatarImage from "../assets/img/avatar.jpg";
+import {Facebook, Instagram, LinkedIn, Portrait, Twitter} from "@material-ui/icons";
+
+const Developer = () => {
+    return (
+        <div className={"Developer"}>
+           
+        </div>
+    );
+}
+
+export default Developer;
